25233a0dc65a511ecdc7f4bf2901d690b2b625b9
Syaiful <pulbagrup@gmail.com>
2024-12-09 14:18:24 +0700

    updatee

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel";
-
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	output: 'static',
-	adapter: vercel(),
+	adapter: vercelStatic(),
 });
